ccc2dffe7c2043bd915373c3c8e99cce716dc5a9
Roberto <aguiarlima.roberto@gmail.com>
2024-01-31 11:07:59 -0300

    Part 2

--- learning_fastapi/main.py
+++ learning_fastapi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -15,3 +16,36 @@ async def post():
 @app.put("/")
 async def put():
     return {"message": "hello from put route"}
+
+
+@app.get('/item/{item_id}')
+async def get_item(item_id: int):
+    return {'item_id': item_id}
+
+@app.get('/users')
+async def get_users():
+    return {'message': "list users route"}
+
+@app.get('/users/{user_id}')
+async def get_user(user_id: int):
+    return {'user_id': user_id}
+
+
+@app.get('/user/me')
+async def get_current_user():
+    return {'message': "This is the current user"}
+
+    
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetable = "vegetable"
+    dairy = "dairy"
+
+@app.get("/food/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetable:
+        return {"food_name": food_name, "message": "you are healthy"}
+    if food_name.value == FoodEnum.fruits:
+        return {"food_name": food_name, "message": "you are still healthy, byt like sweet things"}
+
+    return {"food_name": food_name, "message": "I like chocolate milk"}
